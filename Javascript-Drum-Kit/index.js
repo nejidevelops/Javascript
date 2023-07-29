7129aec3b340233e623b3dd210ab4dfa44240d39
Newton Ombese <newtonombese1@gmail.com>
2023-07-29 08:20:46 +0300

    Add music files

---
+++ Javascript-Drum-Kit/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', function(e) {
+  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play()
+  key.classList.add('playing');
+})
+
+const removeTransition = (e) => {
+  const keys = document.querySelectorAll('.key')
+  keys.forEach(key => {
+    key.classList.remove('playing')
+  })
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition)
+})
